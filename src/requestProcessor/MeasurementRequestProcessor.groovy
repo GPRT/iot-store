301252586c8595ddc0d939710b1c4523c27a0368
Walter Sobral <walter.sobral@gprt.ufpe.br>
2015-08-14 15:51:52 -0300

    Overrided get at MeasurementRequestProcessor for customized para- meters use.

--- src/requestProcessor/MeasurementRequestProcessor.groovy
+++ src/requestProcessor/MeasurementRequestProcessor.groovy
@@ -1,9 +1,54 @@
 package requestProcessor
+import spark.Request
+import spark.Response
 
 import databaseInterfacer.MeasurementInterfacer
+import utils.InputValidator
 
 class MeasurementRequestProcessor extends RequestProcessor {
+
     MeasurementRequestProcessor(factory) {
         super(new MeasurementInterfacer(factory))
+    }
+    public List<LinkedHashMap> get(Request req, Response res) {
+        res.type("application/json");
+
+        Set<String> queryFields = req.queryParams()
+        Set<String> allowedQueryParams = ["fields", "filter", "sort", "expanded", "page", "pageLimit",
+                                          "beginTimestamp", "endTimestamp", "granularity"]
+        InputValidator.validateQueryParams(queryFields, allowedQueryParams)
+
+        def fieldsParam = req.queryParams("fields")
+        def expandedParam = req.queryParams("expanded")
+        def filterFieldsParam = req.queryParams("filter")
+        def sortFieldsParam = req.queryParams("sort")
+        def pageParam = req.queryParams("page")
+        def pageLimitParam = req.queryParams("pageLimit")
+        def beginTimestampParam = req.queryParams("beginTimestamp")
+        def endTimestampParam = req.queryParams("endTimestamp")
+        def granularityParam = req.queryParams("granularity")
+        def networkId = req.params(':id')
+
+        boolean isExpanded = InputValidator.processExpandedParam(expandedParam)
+        Set filterFields = InputValidator.processFilterParam(filterFieldsParam)
+        Set sortFields = InputValidator.processSortParam(sortFieldsParam)
+        int pageField = InputValidator.processPageParam(pageParam)
+        int pageLimitField = InputValidator.processPageLimitParam(pageLimitParam)
+        def beginTimestamp = InputValidator.processTimestampParam(beginTimestampParam)
+        def endTimestamp = InputValidator.processTimestampParam(endTimestampParam)
+        def granularity = InputValidator.processGranularityParam(granularityParam)
+
+        Set allowedFieldNames = null
+        Set listFields = null
+
+        if (isExpanded) {
+            listFields = InputValidator.processListFieldsParam(fieldsParam, this.databaseInterfacer.getExpandedNames())
+        } else {
+            listFields = InputValidator.processListFieldsParam(fieldsParam, this.databaseInterfacer.getFieldNames())
+        }
+
+        return this.databaseInterfacer.get(listFields, filterFields, sortFields, pageField, pageLimitField,
+                                            "Measurements",networkId,beginTimestamp, endTimestamp, granularity)
+
     }
 }
