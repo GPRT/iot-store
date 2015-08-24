d2edaff4e4191daaa623f4ed003a1ec4c58e5449
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-08-24 14:36:39 -0300

    Added an Endpoints class, for rid to url and vice versa conversion. Made endpoints' urls be dynamically generated by Endpoints.

--- src/IotStore.java
+++ src/IotStore.java
@@ -1,5 +1,6 @@
 import com.orientechnologies.orient.core.db.OPartitionedDatabasePoolFactory;
 import requestProcessor.*;
+import utils.Endpoints;
 import utils.JsonTransformer;
 import exceptions.ResponseErrorException;
 
@@ -25,38 +26,50 @@ public class IotStore {
 
         final JsonTransformer jsonTransformer = new JsonTransformer();
 
-        get("/areas", "application/json", areaRequestProcessor::get, jsonTransformer);
-        get("/areas/:id", "application/json", areaRequestProcessor::getById, jsonTransformer);
-        put("/areas/:id", "application/json", areaRequestProcessor::setById, jsonTransformer);
-        delete("/areas/:id", "application/json", areaRequestProcessor::delete, jsonTransformer);
-        post("/areas", "application/json", areaRequestProcessor::create, jsonTransformer);
+        Endpoints.setMainUrl("http://192.168.0.71:4567");
+        Endpoints.addClass("area", "areas");
+        Endpoints.addClass("variable", "variables");
+        Endpoints.addClass("resource", "devices");
+        Endpoints.addClass("group", "groups");
+        Endpoints.addClass("simulation", "simulations");
 
-        get("/variables", "application/json", variableRequestProcessor::get, jsonTransformer);
-        get("/variables/:id", "application/json", variableRequestProcessor::getById, jsonTransformer);
-        put("/variables/:id", "application/json", variableRequestProcessor::setById, jsonTransformer);
-        delete("/variables/:id", "application/json", variableRequestProcessor::delete, jsonTransformer);
-        post("/variables", "application/json", variableRequestProcessor::create, jsonTransformer);
+        String areasPath = Endpoints.getPath("area");
+        get(areasPath, "application / json", areaRequestProcessor::get, jsonTransformer);
+        get(areasPath + "/:id", "application/json", areaRequestProcessor::getById, jsonTransformer);
+        put(areasPath + "/:id", "application/json", areaRequestProcessor::setById, jsonTransformer);
+        delete(areasPath + "/:id", "application/json", areaRequestProcessor::delete, jsonTransformer);
+        post(areasPath, "application/json", areaRequestProcessor::create, jsonTransformer);
 
-        get("/devices", "application/json", deviceRequestProcessor::get, jsonTransformer);
-        get("/devices/:id", "application/json", deviceRequestProcessor::getById, jsonTransformer);
-        put("/devices/:id", "application/json", deviceRequestProcessor::setById, jsonTransformer);
-        delete("/devices/:id", "application/json", deviceRequestProcessor::delete, jsonTransformer);
-        post("/devices", "application/json", deviceRequestProcessor::create, jsonTransformer);
+        String variablesPath = Endpoints.getPath("variable");
+        get(variablesPath, "application/json", variableRequestProcessor::get, jsonTransformer);
+        get(variablesPath + "/:id", "application/json", variableRequestProcessor::getById, jsonTransformer);
+        put(variablesPath + "/:id", "application/json", variableRequestProcessor::setById, jsonTransformer);
+        delete(variablesPath + "/:id", "application/json", variableRequestProcessor::delete, jsonTransformer);
+        post(variablesPath, "application/json", variableRequestProcessor::create, jsonTransformer);
 
-        get("/groups", "application/json", groupRequestProcessor::get, jsonTransformer);
-        get("/groups/:id", "application/json", groupRequestProcessor::getById, jsonTransformer);
-        put("/groups/:id", "application/json", groupRequestProcessor::setById, jsonTransformer);
-        delete("/groups/:id", "application/json", groupRequestProcessor::delete, jsonTransformer);
-        post("/groups", "application/json", groupRequestProcessor::create, jsonTransformer);
+        String devicesPath = Endpoints.getPath("resource");
+        get(devicesPath, "application/json", deviceRequestProcessor::get, jsonTransformer);
+        get(devicesPath + "/:id", "application/json", deviceRequestProcessor::getById, jsonTransformer);
+        put(devicesPath + "/:id", "application/json", deviceRequestProcessor::setById, jsonTransformer);
+        delete(devicesPath + "/:id", "application/json", deviceRequestProcessor::delete, jsonTransformer);
+        post(devicesPath, "application/json", deviceRequestProcessor::create, jsonTransformer);
 
-        get("/simulations", "application/json", simulationRequestProcessor::get, jsonTransformer);
-        get("/simulations/:id", "application/json", simulationRequestProcessor::getById, jsonTransformer);
-        put("/simulations/:id", "application/json", simulationRequestProcessor::setById, jsonTransformer);
-        delete("/simulations/:id", "application/json", simulationRequestProcessor::delete, jsonTransformer);
-        post("/simulations", "application/json", simulationRequestProcessor::create, jsonTransformer);
+        String groupsPath = Endpoints.getPath("group");
+        get(groupsPath, "application/json", groupRequestProcessor::get, jsonTransformer);
+        get(groupsPath + "/:id", "application/json", groupRequestProcessor::getById, jsonTransformer);
+        put(groupsPath + "/:id", "application/json", groupRequestProcessor::setById, jsonTransformer);
+        delete(groupsPath + "/:id", "application/json", groupRequestProcessor::delete, jsonTransformer);
+        post(groupsPath, "application/json", groupRequestProcessor::create, jsonTransformer);
 
-        get("/devices/:id/measurements", "application/json", measurementRequestProcessor::get, jsonTransformer);
-        post("/devices/:id/measurements", "application/json", measurementRequestProcessor::create, jsonTransformer);
+        String simulationsPath = Endpoints.getPath("simulation");
+        get(simulationsPath, "application/json", simulationRequestProcessor::get, jsonTransformer);
+        get(simulationsPath + "/:id", "application/json", simulationRequestProcessor::getById, jsonTransformer);
+        put(simulationsPath + "/:id", "application/json", simulationRequestProcessor::setById, jsonTransformer);
+        delete(simulationsPath + "/:id", "application/json", simulationRequestProcessor::delete, jsonTransformer);
+        post(simulationsPath, "application/json", simulationRequestProcessor::create, jsonTransformer);
+
+        get(devicesPath + "/:id/measurements", "application/json", measurementRequestProcessor::get, jsonTransformer);
+        post(devicesPath + "/:id/measurements", "application/json", measurementRequestProcessor::create, jsonTransformer);
 
         exception(ResponseErrorException.class, (e, req, res) -> {
             ResponseErrorException error = (ResponseErrorException)e;
