e2205764afd23b7f951e5ecf534faf5b11e20a69
Lucas Lira Gomes <x8lucas8x@gmail.com>
2015-08-31 11:45:08 -0300

    Disabled pretty printing in JsonTransformer.

--- src/utils/JsonTransformer.java
+++ src/utils/JsonTransformer.java
@@ -5,12 +5,10 @@ import com.google.gson.GsonBuilder;
 import spark.ResponseTransformer;
 
 public class JsonTransformer implements ResponseTransformer {
-
-    private Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private Gson gson = new GsonBuilder().create();
 
     @Override
     public String render(Object model) {
         return gson.toJson(model);
     }
-
 }
